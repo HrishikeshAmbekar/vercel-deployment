ad5a95eeff8062f70d03839b0e3ee5b7cd57e6c3
HrishikeshAmbekar <ahrishikesh0@gmail.com>
2024-02-01 00:36:25 +0530

    new project

--- server/src/main.ts
+++ server/src/main.ts
@@ -8,18 +8,24 @@
 // bootstrap();
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
+// import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
+// import { CorsModule } from '@nestjs/cors';
+import * as cors from 'cors';
+
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // Enable CORS
-  const corsOptions: CorsOptions = {
-    origin: 'https://vercel-deployment-client-mocha.vercel.app/', // Replace with your client's URL
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-  };
-  app.enableCors(corsOptions);
+  // const corsOptions: CorsOptions = {
+  //   origin: 'https://vercel-deployment-client-mocha.vercel.app/', // Replace with your client's URL
+  //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  //   credentials: true,
+  // };
+  // app.enableCors(corsOptions);
+  app.use(cors());
+
 
   await app.listen(3000);
 }
